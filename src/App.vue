bf11a825faac2ca9a4352732bc8498e0d731c833
reece <reecedevenney@gmail.com>
2023-08-06 14:45:53 -0400

    added cards

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import home from "./views/home.vue"
 </script>
 
 <template>
-  <v-layout>
-    <v-app-bar>
-      <v-row no-gutters class="d-flex align-center">
-        <v-col class="d-flex justify-center">
-          <v-sheet class="ma-2 pa-2">
-            Shop-It!
-          </v-sheet>
-        </v-col>
-        <v-col cols="8" class="d-flex align-center">
-          <input type="text" id="searchbar" class="pl-2 w-100 rounded" placeholder="search" autofocus>
-        </v-col>
-        <v-col class="d-flex justify-center">
-          <v-sheet class="ma-2 pa-2">
-            Loggin
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-app-bar>
-  </v-layout>
+  <v-app>
+    <home></home>
+  </v-app>
 </template>
 
---
+++ src/views/home.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import cardSet from "../components/cardSet.vue"
+</script>
+
+<template>
+    <v-app-bar>
+        <v-row no-gutters class="d-flex align-center">
+            <v-col class="d-flex justify-center">
+                <v-sheet class="ma-2 pa-2">
+                    Shop-It!
+                </v-sheet>
+            </v-col>
+            <v-col cols="8" class="d-flex align-center">
+                <input type="text" id="searchbar" class="pl-2 w-100 rounded" placeholder="search" autofocus>
+            </v-col>
+            <v-col cols="1" class="d-flex justify-center">
+                <v-sheet class="ma-2 pa-2">
+                    Loggin
+                </v-sheet>
+            </v-col>
+            <v-col cols="1" class="d-flex justify-center">
+                <v-sheet class="ma-2 pa-2">
+                    cart
+                </v-sheet>
+            </v-col>
+        </v-row>
+    </v-app-bar>
+    <v-main>
+        <cardSet />
+    </v-main>
+</template>
