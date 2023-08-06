5617f847c8e26da5270b66239f334e6a05a07888
reece <reecedevenney@gmail.com>
2023-08-06 13:54:56 -0400

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,26 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <v-layout>
+    <v-app-bar>
+      <v-row no-gutters class="d-flex align-center">
+        <v-col class="d-flex justify-center">
+          <v-sheet class="ma-2 pa-2">
+            Shop-It!
+          </v-sheet>
+        </v-col>
+        <v-col cols="8" class="d-flex align-center">
+          <input type="text" id="searchbar" class="pl-2 w-100 rounded" placeholder="search" autofocus>
+        </v-col>
+        <v-col class="d-flex justify-center">
+          <v-sheet class="ma-2 pa-2">
+            Loggin
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+  </v-layout>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
