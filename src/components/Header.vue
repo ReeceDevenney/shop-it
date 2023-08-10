36547579e71241bb8e4c84ea49f54d3551c7cf3d
reece <reecedevenney@gmail.com>
2023-08-10 10:46:27 -0400

    moved header code to be seperate component

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+
+let username: string | null = localStorage.getItem("username")
+</script>
+
+<template>
+    <v-app-bar>
+        <v-row no-gutters class="d-flex align-center">
+            <v-col class="d-flex justify-center">
+                <RouterLink to="/" class="ma-2 pa-2 rounded-shaped"
+                    style="text-decoration: none; color: inherit; background-color: green;">
+                    Shop-It!
+                </RouterLink>
+            </v-col>
+            <v-col cols="6" class="d-flex align-center ma-8">
+                <input type="text" id="searchbar" class="pl-2 w-100 rounded" placeholder="search" autofocus>
+            </v-col>
+            <v-col v-if="username" cols="2" class="d-flex justify-center">
+                <RouterLink :to='`/dashboard/${username}`' class="ma-2 pa-2">
+                    <v-btn color="black">{{ username }}</v-btn>
+                </RouterLink>
+            </v-col>
+            <v-col v-else cols="2" class="d-flex justify-center">
+                <RouterLink to="/Loggin" class="ma-2 pa-2">
+                    <v-btn color="black">Loggin</v-btn>
+                </RouterLink>
+            </v-col>
+            <v-col cols="" class="d-flex justify-center">
+                <v-sheet class="ma-2 pa-2">
+                    <v-btn color="black">Cart</v-btn>
+                </v-sheet>
+            </v-col>
+        </v-row>
+    </v-app-bar>
+</template>
--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import cardSet from "../components/cardSet.vue"
+import Header from "../components/Header.vue"
 import { RouterLink } from 'vue-router'
 
 
@@ -7,34 +8,7 @@ let username: string | null = localStorage.getItem("username")
 </script>
 
 <template>
-    <v-app-bar>
-        <v-row no-gutters class="d-flex align-center">
-            <v-col class="d-flex justify-center">
-                <RouterLink to="/" class="ma-2 pa-2 rounded-shaped"
-                    style="text-decoration: none; color: inherit; background-color: green;">
-                    Shop-It!
-                </RouterLink>
-            </v-col>
-            <v-col cols="8" class="d-flex align-center">
-                <input type="text" id="searchbar" class="pl-2 w-100 rounded" placeholder="search" autofocus>
-            </v-col>
-            <v-col v-if="username" cols="1" class="d-flex justify-center">
-                <RouterLink :to='`/dashboard/${username}`' class="ma-2 pa-2">
-                    <v-btn color="black">{{ username }}</v-btn>
-                </RouterLink>
-            </v-col>
-            <v-col v-else cols="1" class="d-flex justify-center">
-                <RouterLink to="/Loggin" class="ma-2 pa-2">
-                    <v-btn color="black">Loggin</v-btn>
-                </RouterLink>
-            </v-col>
-            <v-col cols="1" class="d-flex justify-center">
-                <v-sheet class="ma-2 pa-2">
-                    <v-btn color="black">Cart</v-btn>
-                </v-sheet>
-            </v-col>
-        </v-row>
-    </v-app-bar>
+    <Header />
     <v-main>
         <cardSet />
     </v-main>
