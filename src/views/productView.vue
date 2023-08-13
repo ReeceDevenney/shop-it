9dc85497a359414cacb71a836a992077038ad6a6
reece <reecedevenney@gmail.com>
2023-08-13 12:50:29 -0400

    added basic layout for page

--- src/views/productView.vue
+++ src/views/productView.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import Header from "../components/Header.vue"
+
+</script>
+
 <template>
-    <div>prodcut page</div>
+    <Header />
+    <v-main>
+        <v-row>
+            <v-col cols="3" class="ma-10">
+                <VImg src="https://placehold.co/400x400"></VImg>
+            </v-col>
+            <v-col cols="3" class="ma-10">
+                <h2>Title</h2>
+                <p>price</p>
+                <v-btn>add to cart</v-btn>
+            </v-col>
+        </v-row>
+    </v-main>
 </template>
