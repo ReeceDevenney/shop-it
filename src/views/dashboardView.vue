369864ea6b15f01144ec30c8686b862564935910
reece <reecedevenney@gmail.com>
2023-08-18 11:48:58 -0400

    users can only access their personal dashboard

--- src/views/dashboardView.vue
+++ src/views/dashboardView.vue
@@ -1,22 +1,48 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, onBeforeMount, onMounted } from "vue"
 import Header from "../components/Header.vue"
 import ProductCard from "@/components/ProductCard.vue";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 import db from '../firebaseInit'
-
-const products = ["this is some sample text", "longet text", "testing testing", "one more test", "and another one", "asdfasdfasdf"]
-
+// gets userId from the URL
 const url = window.location.href
 const split = url.split("/")
+const userId = split[split.length - 1]
+
+const auth = getAuth();
+let user = ref(auth.currentUser);
+onBeforeMount(() => onAuthStateChanged(auth, (users) => {
+    if (users?.uid != userId) {
+        window.location.href = '/'
+    }
+}))
+
+
+
+// pulls any products posted by the user
+let products: any = ref([])
+onMounted(async () => {
+    const querySnapshot = await getDocs(query(collection(db, "Products"), where("postedBy", "==", userId)));
+    let productsTemp: any = []
+    querySnapshot.forEach((doc) => {
+        const product = {
+            id: doc.id,
+            productName: doc.data().productName,
+            price: doc.data().price,
+        }
+        productsTemp.push(product)
+    })
+    products.value = productsTemp
+})
 
 let productName = ref('')
 let imageUrl = ref('')
 let price = ref<number | null>()
 let description = ref('')
-const userId = split[split.length - 1]
 
-const printWords = async () => {
+// lets a user post a new product
+const addProduct = async () => {
     const docRef = await addDoc(collection(db, "Products"), {
         postedBy: userId,
         productName: productName.value,
@@ -43,8 +69,8 @@ const priceRules = [
     <v-main>
         <h2 class="d-flex justify-center ma-2">Your Products</h2>
         <v-row class="d-flex justify-center ma-2">
-            <v-col v-for="(product, index) in products" cols="2">
-                <ProductCard :product="product" :index="index" />
+            <v-col v-for="product in products" cols="2">
+                <ProductCard :product="product?.productName" :index="product?.id" :price="product?.price" />
                 <div class="d-flex justify-space-around">
                     <v-btn class="ma-1 bg-yellow-lighten-2">edit</v-btn>
                     <v-btn class="ma-1 bg-red-darken-4">delete</v-btn>
@@ -69,7 +95,7 @@ const priceRules = [
                         <v-text-field label="Price" v-model="price" :rules="priceRules" required></v-text-field>
                     </v-col>
                 </v-row>
-                <v-btn class="bg-green" @click="printWords">Add Product</v-btn>
+                <v-btn class="bg-green" @click="addProduct">Add Product</v-btn>
             </v-container>
         </v-form>
     </v-main>
