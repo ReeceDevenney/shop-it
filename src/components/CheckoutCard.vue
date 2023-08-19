8d553ac4668792fae697f294044132f443c8d539
reece <reecedevenney@gmail.com>
2023-08-19 10:13:41 -0400

    cart items now show up in cart page

--- src/components/CheckoutCard.vue
+++ src/components/CheckoutCard.vue
@@ -1,13 +1,17 @@
+<script setup lang="ts">
+defineProps(["imageUrl", "productName", "price"])
+</script>
+
 <template>
     <v-row class="d-flex justify-space-between my-2 mx-16 pa-2">
         <v-col cols="2">
-            <v-img src="https://placehold.co/400x400"></v-img>
+            <v-img :src="imageUrl"></v-img>
         </v-col>
         <v-col cols="7" class="mr-16">
-            <p>Product Name</p>
+            <p>{{ productName }}</p>
         </v-col>
         <v-col cols="2">
-            <p>Product Price</p>
+            <p>${{ price }}</p>
         </v-col>
         <v-divider></v-divider>
     </v-row>
--- src/views/cartView.vue
+++ src/views/cartView.vue
@@ -2,11 +2,13 @@
 import Header from "../components/Header.vue"
 import CheckoutCard from "../components/CheckoutCard.vue"
 
+const cart = JSON.parse(localStorage.getItem("cart"))
+console.log(cart[0].imageUrl)
 </script>
 
 <template>
     <Header />
     <v-main>
-        <CheckoutCard v-for="n in 4" />
+        <CheckoutCard v-for="item in cart" :imageUrl="item.imageUrl" :productName="item.productName" :price="item.price" />
     </v-main>
 </template>
