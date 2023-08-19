0deb4f4235449ed8342b8865e34b2fb0ae6475f2
reece <reecedevenney@gmail.com>
2023-08-19 10:23:30 -0400

    cart page still renders if nothing is in cart

--- src/views/cartView.vue
+++ src/views/cartView.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import Header from "../components/Header.vue"
 import CheckoutCard from "../components/CheckoutCard.vue"
+let test = localStorage.getItem("cart")
+let cart = []
+if (test) {
+    cart = JSON.parse(test)
+}
 
-const cart = JSON.parse(localStorage.getItem("cart"))
-console.log(cart[0].imageUrl)
 </script>
 
 <template>
     <Header />
     <v-main>
         <CheckoutCard v-for="item in cart" :imageUrl="item.imageUrl" :productName="item.productName" :price="item.price" />
+        <div v-if="!cart[0]" class="d-flex justify-center ma-4">no items in cart</div>
     </v-main>
 </template>
