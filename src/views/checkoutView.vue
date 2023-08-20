1c47abc8af0bf6a95ee90b8fd60f8739192a8b7c
reece <reecedevenney@gmail.com>
2023-08-20 12:28:41 -0400

    added address form to checkout page

--- src/views/checkoutView.vue
+++ src/views/checkoutView.vue
@@ -4,7 +4,23 @@ import Header from "../components/Header.vue"
 
 <template>
     <Header />
-    <v-main>
-        <div>testing</div>
+    <v-main class="mt-16">
+        <div class="d-flex flex-direction: row justify-center">
+            <div class="d-flex justify-center ma-2">
+                <input placeholder="City" class="rounded">
+            </div>
+            <div class="d-flex justify-center ma-2">
+                <input placeholder="State" class="rounded">
+            </div>
+        </div>
+        <div class="d-flex justify-center mb-2">
+            <input placeholder="Address" class="w-25 rounded">
+        </div>
+        <div class="d-flex justify-center mb-2">
+            <input placeholder="Zip-Code" class="w-25 rounded">
+        </div>
+        <div class="d-flex justify-center">
+            <v-btn class="ma-auto bg-green-darken-1">Checkout</v-btn>
+        </div>
     </v-main>
 </template>
